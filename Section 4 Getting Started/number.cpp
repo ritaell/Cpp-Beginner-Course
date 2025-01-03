eaa287051c5c8a841394345066e927469c043dff
rita <rita.ell2009@gmail.com>
2025-01-03 16:34:38 +0200

    lecture 27

---
+++ Section 4 Getting Started/number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{   
+    int fav_num;
+    std::cout<<"Enter your fav number between 1 and 100: ";
+    std::cin>> fav_num;
+    std::cout<<"Amazing! That's my favourite number too."<<std::endl;
+    return 0;
+}
