b419f78d4dc0f7a1e35a5297bf7cd40ab3a9dc28
rita <rita.ell2009@gmail.com>
2025-01-08 22:46:41 +0200

    challenge s4

---
+++ Section 4 Getting Started/challenge.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//version1
+int main(){
+int fav_num;
+std::cout<<"Enter your favourite number between 1-100:  ";
+std::cin>>fav_num;
+std::cout<<"No really!!, "<<fav_num<<" is my favourite number!";
+
+}
+
